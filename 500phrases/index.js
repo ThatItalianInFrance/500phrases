db1d83ccd859410a1e815f4d5b88b0b93a7f9505
ThatItalianInFrance <filippobasile.r@gmail.com>
2023-04-10 18:27:35 +0200

    first commit

---
+++ 500phrases/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const eta = require("eta");
+const fs = require("fs");
+
+app.engine("eta", eta.renderFile);
+eta.configure({ views: "./views", cache: true });
+app.set("views", "./views");
+app.set("view cache", true);
+app.set("view engine", "eta");
+app.use(express.static("assets"));
+
+const jsonPath = "phrases.json";
+
+function loadPhrases() {
+  if (!fs.existsSync(jsonPath)) fs.writeFileSync(jsonPath, "[]", "utf8");
+  let todosObj = JSON.parse(fs.readFileSync(jsonPath, "utf8"));
+  if (!Array.isArray(todosObj)) todosObj = []; // ici petite sécurité pour éviter les erreurs si le fichier JSON est corrompu (si ce n'est pas un tableau)
+  return todosObj;
+}
+let phrases = loadPhrases();
+
+app.get("/", function (req, res) {
+  res.render("index.eta", { phrases });
+});
+app.get("/:id", function (req, res) {
+  id = req.params.id;
+  res.render("phrase.eta", { id, phrases });
+});
+app.listen(8000, function () {
+  console.log("listening on port 8000");
+});
